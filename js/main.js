e15cebd58f73f2d6869bf247ddc254b4d5befe78
JDSerna00 <jdserna00@gmail.com>
2021-05-17 23:55:10 -0500

    Corregida la lógica

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
-var tiro1;
-var tiro2;
+var tiro1 = 0;
+var tiro2 = 0;
 var posiciones = [0, -160, -320, -481, -642, -803];
 var dado1, dado2, botonTirar;
-var turno = 0
+var turno = 0;
+var puntos = 0;
 window.onload = init;
 
 function init() {
-    botonTirar = document.getElementById("botonTirar");
+    botonTirar = document.getElementById('botonTirar');
 
-    dado1 = document.getElementById("dado1");
-    dado2 = document.getElementById("dado2");
+    dado1 = document.getElementById('dado1');
+    dado2 = document.getElementById('dado2');
 }
 
 function tirardado() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+function reiniciar() {
+    tiro1 = 0;
+    tiro2 = 0;
+    turno = 0;
+    puntos = 0;
+    
+    document.getElementById('puntos').innerHTML = puntos;
+    document.getElementById('resultado').innerHTML = '';
+    document.getElementById('boton_tirar').disabled = false;
+}
+
 function actualizarDado(ref, cara) {
-    ref.style.backgroundPosition = posiciones[cara - 1] + "px";
+    ref.style.backgroundPosition = posiciones[cara - 1] + 'px';
 }
 
 function jugar() {
@@ -31,21 +43,33 @@ function jugar() {
     var resultado = tiro1 + tiro2;
     var textoResultado = '';
 
+    if (puntos != 0) {
+        if (resultado == 7) { 
+            textoResultado += `Shame buddy, you lost`;
+            document.getElementById('boton_tirar').disabled = true;
+        } else if (puntos == resultado) {
+            textoResultado += `Hey how lucky, you won`;
+            document.getElementById('boton_tirar').disabled = true;
+        }
+        
+        document.getElementById('resultado').innerHTML = textoResultado;
+    } else {
+        if (resultado == 7 || resultado == 11) { 
+            textoResultado += `Hey how lucky, you won`;
+            document.getElementById('boton_tirar').disabled = true;
+        }
+    
+        if (resultado == 2 || resultado == 3 || resultado == 12) {
+            textoResultado += `Shame buddy, you lost`;
+            document.getElementById('boton_tirar').disabled = true;
+        }
+    
+        if (resultado == 4 || resultado == 5 || resultado == 6 || resultado == 8 || resultado == 9 || resultado == 10) {
+            textoResultado += `You have set a point`;
+            puntos = resultado;
+            document.getElementById('puntos').innerHTML = puntos;
+        }
 
-    if (resultado == 7 || resultado == 11) { 
-        // textoResultado = textoResultado + 'Hey how lucky, you won'; 
-        textoResultado += `Turno ${turno} Hey how lucky, you won`;
+        document.getElementById('resultado').innerHTML = textoResultado;
     }
-
-    if (resultado == 2 || resultado == 3 || resultado == 12) {
-        // textoResultado =  textoResultado + 'Shame buddy, you lost';
-        textoResultado += `Turno ${turno} Shame buddy, you lost`;
-    }
-
-    if (resultado == 4 || resultado == 5 || resultado == 6 || resultado == 8 || resultado == 9 || resultado == 10) {
-        // textoResultado = textoResultado + 'Congrats! You scored a point';
-        textoResultado += `Turno ${turno} Congrats! You scored a point`;
-    }
-
-    document.getElementById('resultado').innerHTML = textoResultado;
 }
